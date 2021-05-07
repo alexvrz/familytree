cd9ffceb1dc7d57b14520b7440ff432ea263a476
alexvrz <alexvrz2357@gmail.com>
2021-05-07 12:30:23 +0000

    done

--- src/models.py
+++ src/models.py
@@ -2,18 +2,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+# association table for children<->parents
+children = db.Table('children',
+                db.Column('child_id', db.Integer, db.ForeignKey('member.id')),
+                db.Column('parent_id', db.Integer, db.ForeignKey('member.id'))
+                )
 
+class Member(db.Model):
+    """Node within the family tree."""
+
+    __tablename__ = "member"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    lastname = db.Column(db.String(40), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    parents = db.relationship('Member',
+                          secondary=children,
+                          primaryjoin=(children.c.child_id == id), # primaryjoin means left side of association table is child_id
+                          secondaryjoin=(children.c.parent_id == id), # secondaryjoin means right side of association table is parent_id
+                          backref=db.backref('children', lazy='dynamic'),
+                          lazy='dynamic')
+
+                          # for testing relationships, for example to search for the childs of an specific member_id, run:
+                          # $ mysql
+                          # USE example;
+                          # SHOW TABLES;  (to verify tables names in current database 'example')
+                          # SELECT * FROM member, children WHERE member.id = children.parent_id;
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Member %r>' % self.name
 
-    def serialize(self):
+    def serialize(self, member_type): # add member_type parameter to serialize member_type
         return {
+            member_type : {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "lastname": self.lastname,
+            "age": self.age
+            }
         }
+        
